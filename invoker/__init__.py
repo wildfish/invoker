87769c7d5f932b880bdd1f752d5843e2832db1f5
Dan Bate <dan.bate@gmail.com>
2018-10-25 10:39:44 +0100

    added setup configs

--- invoker/__init__.py
+++ invoker/__init__.py
@@ -1,3 +1,4 @@
 from .invoker import invoker
 
 __all__ = ['invoker']
+__version__ = '0.0.0'
---
+++ setup.py
@@ -0,0 +1,98 @@
+from __future__ import print_function, division
+
+import glob
+import os
+import io
+import re
+import shutil
+import sys
+import tarfile
+from contextlib import contextmanager
+from distutils.log import INFO, WARN, ERROR
+from distutils.spawn import find_executable
+from tempfile import mkdtemp
+from time import sleep
+
+from setuptools import find_packages, setup, Command
+
+SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_readme():
+    with io.open(os.path.join(SCRIPT_DIR, 'README.rst'), encoding='utf-8') as readme:
+        return readme.read()
+
+
+def get_install_requirements():
+    with io.open(os.path.join(SCRIPT_DIR, 'requirements-package.in'), encoding='utf-8') as reqs:
+        return reqs.readlines()
+
+
+def get_version():
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    with io.open(os.path.join(SCRIPT_DIR, 'invoker', '__init__.py'), encoding='utf-8') as init_py:
+        return re.search('__version__ = [\'"]([^\'"]+)[\'"]', init_py.read()).group(1)
+
+
+version = get_version()
+reqs = get_install_requirements()
+readme = get_readme()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+
+class Publish(Command):
+    command_name = 'publish'
+
+    def run(self):
+        if os.system('pip freeze | grep twine'):
+            self.announce('twine not installed.\nUse `pip install twine`.\nExiting.', WARN)
+            sys.exit(1)
+
+        os.system('python setup.py sdist')
+        os.system('python setup.py bdist_wheel')
+
+        os.system('twine upload dist/*')
+
+
+setup(
+    name='invoker',
+    version=version,
+    packages=find_packages(exclude=('tests', 'tests.*', 'tests.*.*')),
+    include_package_data=True,
+    package_data={
+        '': [
+            'requirements-package.in',
+            'LICENSE',
+        ],
+    },
+    exclude_package_data={
+        '': ['__pycache__', '*.py[co]'],
+    },
+    license='BSD 3-Clause',
+    description='A wrapper around invoke to help facilitate sharing tasks and specifying multiple environments',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url='https://github.com/wildfish/invoker',
+    author='Wildfish',
+    author_email='Wildfish <developer@wildfish.com>',
+    keywords='invoke invoker',
+    install_requires=reqs,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    cmdclass={
+        'publish': Publish,
+    },
+)
